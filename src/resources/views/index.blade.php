2db0b3a40c1d84471b31655b650acbc3ded86bbb
kichi <9djz.96@gmail.com>
2023-02-09 06:56:52 +0900

    public cssに変更

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -23,7 +23,9 @@
   <!-- スタイルシートはここから -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  @vite(['resources/css/style.css', 'resources/css/queries.css'])
+  {{-- @vite(['resources/css/style.css', 'resources/css/queries.css']) --}}
+  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/queries.css') }}">
 </head>
 
 <body>
